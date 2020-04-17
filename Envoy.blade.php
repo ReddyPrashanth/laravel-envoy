9821360b003726bd266d4de0d3e6f6a29e654e68
Prashanth Sreepathi <s.prasantreddy94@gmail.com>
2020-04-16 19:48:16 -0500

    complete laravel setup

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,37 @@
 @servers(['web' => ['dropletps']])
 
 
-@task('list', ['on'=>'web'])
-	ls -la
+@task('git')
+	cd /home/psreepathi/laravel-projects/laravel-envoy
+	git pull origin master
 @endtask
+
+
+@task('composer')
+	cd /home/psreepathi/laravel-projects/laravel-envoy
+	composer install --no-interaction --prefer-dist --optimize-autoloader
+@endtask
+
+
+@task('npm')
+	cd /home/psreepathi/laravel-projects/laravel-envoy
+	npm install
+	npm rundev
+@endtask
+
+
+@task('migrate')
+	cd /home/psreepathi/laravel-projects/laravel-envoy
+	
+	if [ -f artisan]
+	then
+		php artisan migrate --force 
+	fi
+@endtask
+
+@story('deploy')
+	git
+	composer
+	npm
+	migrate
+@endstory
